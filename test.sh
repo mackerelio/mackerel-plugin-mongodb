6f7ce5e2899beffb614d01798e7d153a6526b8d4
yseto <yseto@users.noreply.github.com>
2023-02-06 10:45:22 +0900

    added multiple version tests.

--- test.sh
+++ test.sh
@@ -19,18 +19,27 @@ fi
 user=root
 password=passpass
 port=27017
-docker run -d \
+
+for v in 6.0 5.0 4.4 4.2 4.0 3.6
+do
+  docker run -d \
 	--name test-$plugin \
 	-p $port:$port \
 	-e MONGO_INITDB_ROOT_USERNAME=$user \
 	-e MONGO_INITDB_ROOT_PASSWORD=$password \
-	mongo:6.0.4
-trap 'docker stop test-$plugin; docker rm test-$plugin; exit 1' 1 2 3 15
-sleep 10
+	mongo:$v
+  trap 'docker stop test-$plugin; docker rm test-$plugin; exit 1' 1 2 3 15
+  sleep 10
 
-$plugin -port $port -username=$user -password $password
-status=$?
+  if $plugin -port $port -username=$user -password $password
+  then
+    echo OK: $v
+  else
+    status=$?
+    echo NG: $v
+  fi
 
-docker stop "test-$plugin"
-docker rm "test-$plugin"
+  docker stop "test-$plugin"
+  docker rm "test-$plugin"
+done
 exit $status
