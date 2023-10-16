d82369b2e93533fcac5ca48deabc59f5a465e4cf
lufia <lufia@lufia.org>
2023-10-16 16:37:47 +0900

    fix shellcheck warnings

--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-prog=$(basename $0)
+prog=$(basename "$0")
 if ! [ -S /var/run/docker.sock ]
 then
 	echo "$prog: there are no running docker" >&2
 	exit 2
 fi
 
-cd $(dirname $0)
+cd "$(dirname "$0")" || exit
 PATH=$(pwd):$PATH
-plugin=$(basename $(pwd))
-if ! which $plugin >/dev/null
+plugin=$(basename "$(pwd)")
+if ! which "$plugin" >/dev/null
 then
 	echo "$prog: $plugin is not installed" >&2
 	exit 2
@@ -19,11 +19,12 @@ fi
 user=root
 password=passpass
 port=27017
+status=0
 
 for v in 6.0 5.0 4.4 4.2 4.0 3.6
 do
   docker run -d \
-	--name test-$plugin \
+	--name "test-$plugin" \
 	-p $port:$port \
 	-e MONGO_INITDB_ROOT_USERNAME=$user \
 	-e MONGO_INITDB_ROOT_PASSWORD=$password \
@@ -53,4 +54,4 @@ do
   docker stop "test-$plugin"
   docker rm "test-$plugin"
 done
-exit $status
+exit "$status"
