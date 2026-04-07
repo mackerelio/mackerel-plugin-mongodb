7a647bee87756087243ed277394d1db52f9664bf
yseto <yseto@users.noreply.github.com>
2026-04-07 14:07:28 +0900

    mod upgrade --mod-name=go.mongodb.org/mongo-driver
    https://github.com/mongodb/mongo-go-driver/blob/master/docs/migration-2.0.md

--- lib/mongodb_test.go
+++ lib/mongodb_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 func TestGraphDefinition(t *testing.T) {
